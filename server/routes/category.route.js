942f975fdf813a62167717de8b333b6d6a369336
mr-Ayalk <ayalkteketel7@gmail.com>
2025-08-22 10:31:19 +0300

    category route api copleted

--- server/routes/category.route.js
+++ server/routes/category.route.js
@@ -3,8 +3,13 @@ import auth from "../middlewares/auth.js";
 import upload from "../middlewares/muller.js";
 import {
   createCategory,
+  deleteCatagory,
+  getCategories,
   getCategory,
   getCategoryCount,
+  getsubCategoryCount,
+  removeImageFromCloudinary,
+  updatedCategory,
   uploadImages,
 } from "../controller/category.controller.js";
 
@@ -17,6 +22,13 @@ categoryRouter.post(
 );
 
 categoryRouter.post("/create", auth, createCategory);
-categoryRouter.get("/", auth, getCategory);
-categoryRouter.get("/get/count", auth, getCategoryCount);
+categoryRouter.get("/", getCategories);
+categoryRouter.get("/get/count", getCategoryCount);
+categoryRouter.get("/get/count/subCat", getsubCategoryCount);
+categoryRouter.get("/:id", getCategory);
+categoryRouter.delete("/deleteImage", auth, removeImageFromCloudinary);
+categoryRouter.delete("/:id", auth, deleteCatagory);
+// categoryRouter.put("/:id", auth, updatedCategory);
+categoryRouter.put("/:id", auth, upload.array("images"), updatedCategory);
+
 export default categoryRouter;
