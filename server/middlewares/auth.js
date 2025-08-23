736e7879516260e17844ce963cc3394a1f431d96
mr-Ayalk <ayalkteketel7@gmail.com>
2025-08-23 08:10:22 +0300

    cart api completed

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,7 +3,7 @@ const auth = async (request, response, next) => {
   try {
     const token =
       request.cookies.accessToken ||
-      request?.headers?.authorization?.split("")[1];
+      request?.headers?.authorization?.split(" ")[1];
 
     if (!token) {
       return response.status(401).json({
@@ -33,3 +33,46 @@ const auth = async (request, response, next) => {
 };
 
 export default auth;
+
+// import jwt from "jsonwebtoken";
+
+// const auth = async (req, res, next) => {
+//   try {
+//     // Log headers & cookies
+//     console.log("Headers:", req.headers);
+//     console.log("Cookies:", req.cookies);
+
+//     // Extract token
+//     const token =
+//       req.cookies?.accessToken ||
+//       (req.headers?.authorization?.startsWith("Bearer ")
+//         ? req.headers.authorization.split(" ")[1]
+//         : null);
+
+//     if (!token) {
+//       console.log("No token provided");
+//       return res.status(401).json({
+//         message: "Provide token",
+//         error: true,
+//         success: false,
+//       });
+//     }
+
+//     // Verify token
+//     const decode = jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN);
+//     req.userId = decode.id || decode.userId;
+
+//     console.log("UserId from auth middleware:", req.userId);
+
+//     next();
+//   } catch (error) {
+//     console.error("Auth middleware error:", error.message);
+//     return res.status(401).json({
+//       message: error.message || error,
+//       error: true,
+//       success: false,
+//     });
+//   }
+// };
+
+// export default auth;
--- server/routes/cart.route.js
+++ server/routes/cart.route.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import auth from "../middlewares/auth.js";
-import upload from "../middlewares/muller.js";
 import {
   addToCartItemController,
+  deleteCartItemWtyController,
   getCartItemController,
+  updateCartItemQtyController,
 } from "../controller/cart.controller.js";
 
 const cartRoute = Router();
 
 cartRoute.post("/add", auth, addToCartItemController);
 cartRoute.get("/get", auth, getCartItemController);
-
+cartRoute.put("/update-qty",auth,updateCartItemQtyController);
+cartRoute.delete("/deleteCartItem",auth,deleteCartItemWtyController)
 export default cartRoute;
