37a9339f4b898e0ce596168c44b89c572ebec3c8
mr-Ayalk <ayalkteketel7@gmail.com>
2025-10-06 23:06:27 +0300

    server logic for adding weight and size completed

---
+++ server/models/productWEIGHT.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const productWEIGHTSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ProductWEIGHTModel = mongoose.model("ProductWEIGHT", productWEIGHTSchema);
+
+export default ProductWEIGHTModel;
