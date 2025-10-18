cc96413ab9a58dd6ed8cab748345752710bb099e
mr-Ayalk <ayalkteketel7@gmail.com>
2025-10-18 23:10:42 +0300

    filter by category and rating completed

--- Users/src/Pages/ProductListing/ProductListing.jsx
+++ Users/src/Pages/ProductListing/ProductListing.jsx
@@ -368,7 +368,7 @@ function ProductListing() {
             <div
               className={`grid ${
                 itemView === "grid"
-                  ? "grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4"
+                  ? "grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4"
                   : "grid-cols-1"
               }`}
             >
