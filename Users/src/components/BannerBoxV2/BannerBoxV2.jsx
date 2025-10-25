ef6e638b7a538f5c14676a6e49ea20068614536d
mr-Ayalk <ayalkteketel7@gmail.com>
2025-10-25 14:46:13 +0300

    banner section completed

--- Users/src/components/BannerBoxV2/BannerBoxV2.jsx
+++ Users/src/components/BannerBoxV2/BannerBoxV2.jsx
@@ -3,28 +3,27 @@ import { Link } from "react-router-dom";
 function BannerBoxV2(props) {
   return (
     <div className="bannerBoxV2 w-full overflow-hidden rounded-md group relative !h-[210px]">
-      <img
-        src={props.img}
-        alt=""
-        className="w-full transition-all duration-150 group-hover:scale-105   h-full object-cover"
-      />
+      {props.data?.images?.[0]?.url && (
+        <img
+          src={props.data.images[0].url}
+          alt="banner"
+          className="w-full h-full object-cover transition-all duration-150 group-hover:scale-105"
+        />
+      )}
+
       <div
         className={`info absolute p-5 top-0 ${
           props.info === "left" ? "left-0" : "right-0"
-        } w-[70%] h-[100%] z-50 flex items-center justify-center flex-col gap-2
-        ${props.info === "left" ? "" : "pl-24"}
-        `}
+        } w-[70%] h-full z-50 flex items-center justify-center flex-col gap-2 ${
+          props.info === "left" ? "" : "pl-24"
+        }`}
       >
         <h2 className="text-[18px] font-[600]">{props.data?.bannerTitle}</h2>
         <span className="text-[20px] text-primary font-[600] w-full">
-          {/* ${props.data?.price} */}1$ only
-        </span>
-        <span className="text-[20px]  font-[600] w-full">
-          {/* ${props.data?.price} */}
-          78% Discount Welcome Deal
+          ${props.data?.price}
         </span>
         <div className="w-full">
-          <Link to="/" className="text-[16px] font-[600] link text-primary">
+          <Link to="/" className="text-[16px] font-[600] link">
             SHOP NOW
           </Link>
         </div>
