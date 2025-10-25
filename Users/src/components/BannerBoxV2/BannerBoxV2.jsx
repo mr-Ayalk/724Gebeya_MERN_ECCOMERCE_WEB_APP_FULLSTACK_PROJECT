921a23b381bdd72c50f7ef76abed659946a52b6a
mr-Ayalk <ayalkteketel7@gmail.com>
2025-10-25 14:50:50 +0300

    banner section updated again

--- Users/src/components/BannerBoxV2/BannerBoxV2.jsx
+++ Users/src/components/BannerBoxV2/BannerBoxV2.jsx
@@ -14,16 +14,17 @@ function BannerBoxV2(props) {
       <div
         className={`info absolute p-5 top-0 ${
           props.info === "left" ? "left-0" : "right-0"
-        } w-[70%] h-full z-50 flex items-center justify-center flex-col gap-2 ${
+        } w-[70%] h-full  flex items-center justify-center flex-col gap-2 ${
           props.info === "left" ? "" : "pl-24"
         }`}
       >
-        <h2 className="text-[18px] font-[600]">{props.data?.bannerTitle}</h2>
+        {" "}
         <span className="text-[20px] text-primary font-[600] w-full">
           ${props.data?.price}
         </span>
+        <h2 className="text-[18px] font-[600]">{props.data?.bannerTitle}</h2>
         <div className="w-full">
-          <Link to="/" className="text-[16px] font-[600] link">
+          <Link to="/" className="text-[16px] font-[600] link text-primary">
             SHOP NOW
           </Link>
         </div>
